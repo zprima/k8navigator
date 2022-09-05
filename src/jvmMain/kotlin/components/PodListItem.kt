33393df30981940a5f704d8b8b75c112806e69bd
Primož Žnidar <sg.prima@gmail.com>
2022-09-05 23:12:36 +0200

    Update PodListItem.kt

--- src/jvmMain/kotlin/components/PodListItem.kt
+++ src/jvmMain/kotlin/components/PodListItem.kt
@@ -24,6 +24,51 @@ import model.*
 import java.util.Calendar
 
 @Composable
+fun PodListItem(pod: K8Pod, sshCommand: suspend (String) -> Unit, logsCommand: suspend (String) -> Unit) {
+    val scope = rememberCoroutineScope()
+    val podColor = if(pod.status.phase == "Running") Color(0xFF008080) else Color.Black
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 16.dp)
+            .border(1.dp, Color.LightGray)
+            .padding(horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            SelectionContainer {
+                Text(pod.metadata.name, style = TextStyle(color = podColor))
+            }
+
+            Row(
+                horizontalArrangement = Arrangement.End
+            ) {
+                TextButton(onClick = { scope.launch { sshCommand(pod.metadata.name) }}) {
+                    Text("ssh")
+                }
+
+                TextButton(onClick = { scope.launch { logsCommand(pod.metadata.name) }}) {
+                    Text("logs")
+                }
+            }
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text("${pod.status.phase} in ${pod.metadata.namespace}", fontSize = 12.sp)
+            Text(pod.status.startTime, fontSize = 12.sp)
+        }
+    }
+}
+
+@Composable
 @Preview
 fun PodListItemPreview(){
     val podMetadata = K8PodMetadata(
@@ -56,50 +101,4 @@ fun PodListItemPreview(){
         sshCommand = {},
         logsCommand = {}
     )
-}
-
-@Composable
-fun PodListItem(pod: K8Pod, sshCommand: suspend (String) -> Unit, logsCommand: suspend (String) -> Unit) {
-    val scope = rememberCoroutineScope()
-
-    Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(bottom = 16.dp)
-            .border(1.dp, Color.LightGray)
-            .padding(horizontal = 8.dp)
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-
-            val podColor = if(pod.status.phase == "Running") Color(0xFF008080) else Color.Black
-
-            SelectionContainer {
-                Text(pod.metadata.name, style = TextStyle(color = podColor))
-            }
-
-            Row(
-                horizontalArrangement = Arrangement.End
-            ) {
-                TextButton(onClick = { scope.launch { sshCommand(pod.metadata.name) }}) {
-                    Text("ssh")
-                }
-
-                TextButton(onClick = { scope.launch { logsCommand(pod.metadata.name) }}) {
-                    Text("logs")
-                }
-            }
-        }
-
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text("${pod.status.phase} in ${pod.metadata.namespace}", fontSize = 12.sp)
-            Text(pod.status.startTime, fontSize = 12.sp)
-        }
-    }
 }
